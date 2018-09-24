94b22ad6c1f4edbf1d86d9e5ecc97fe2aff4aeb0
Sarang Joshi <sarangj@msn.com>
2018-09-24 02:33:08 -0700

    Continue work on parsing

--- server/server.go
+++ server/server.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"math"
 	"net/http"
 	"regexp"
 	"strconv"
@@ -10,6 +11,13 @@ import (
 	"github.com/msoap/byline"
 	"golang.org/x/net/html"
 )
+
+// AvailabilityResponse represents the response to the GetAvailability request
+type AvailabilityResponse struct {
+	avail          []availability
+	startTimestamp uint64
+	nOfTimeslots   uint64 // # of 15 minute intervals
+}
 
 type availability struct {
 	id        int
@@ -35,14 +43,14 @@ type instance struct {
 }
 
 // Returns availability, start timestamp, and number of 15-minute slots
-func getAvailability(i instance) (avail []availability, startTimestamp uint64, nOfTimeslots uint64) {
+func getAvailability(i instance) (ar AvailabilityResponse, err error) {
 	url := fmt.Sprintf("https://when2meet.com/AvailabilityGrids.php?id=%d&code=%s", i.id, i.code)
 
 	resp, err := http.Get(url)
 	if err != nil {
 		// handle error
 		fmt.Println(err)
-		return nil, 0, 0
+		return
 	}
 	defer resp.Body.Close()
 
@@ -50,117 +58,90 @@ func getAvailability(i instance) (avail []availability, startTimestamp uint64, n
 	state := start
 
 	// Tokenize
-	shouldTokenize := false
-	if shouldTokenize {
-		z := html.NewTokenizer(resp.Body)
+	doc, err := html.Parse(resp.Body)
 
-		for {
-			tagType := z.Next()
-			if tagType == html.ErrorToken {
-				// error
-				fmt.Println("tokenizing error")
-				return nil, 0, 0
-			} else if tagType == html.StartTagToken {
-				tagName, _ := z.TagName()
-				if string(tagName) != "script" && availabilityJs == nil {
-					state = tokenizerReadScript
-				}
-			} else if tagType == html.TextToken {
-				if state == tokenizerReadScript {
-					content := string(z.Text())
-					availabilityJs = strings.Split(content, "\n")
+	if err != nil {
+		// error
+		return ar, err
+	}
+	// Returns whether to continue parsing the tree
+	var f func(*html.Node) (bool, error)
+	f = func(n *html.Node) (bool, error) {
+		switch state {
+		case start:
+			if n.Type == html.ElementNode && n.Data == "script" {
+				state = parserFoundScript
+			}
+			break
+		case parserFoundScript:
+			if n.Type == html.TextNode {
+				content := string(n.Data)
+				availabilityJs = strings.Split(content, "\n")
+				state = parserLookingForSlotsStart
+			}
+			break
+		case parserLookingForSlotsStart:
+			if n.Type == html.ElementNode && n.Data == "div" {
+				for _, attr := range n.Attr {
+					if attr.Key == "id" && attr.Val == "YouGridSlots" {
+						state = parserFoundSlotsStart
+					}
 				}
 			}
-		}
-	} else {
-		doc, err := html.Parse(resp.Body)
-
-		if err != nil {
-			// error
-			fmt.Println(err)
-			return nil, 0, 0
-		}
-		// Returns whether to continue parsing the tree
-		var f func(*html.Node) (bool, error)
-		f = func(n *html.Node) (bool, error) {
-			switch state {
-			case start:
-				if n.Type == html.ElementNode && n.Data == "script" {
-					state = parserFoundScript
-				}
-				break
-			case parserFoundScript:
-				if n.Type == html.TextNode {
-					content := string(n.Data)
-					availabilityJs = strings.Split(content, "\n")
-					state = parserLookingForSlotsStart
-				}
-				break
-			case parserLookingForSlotsStart:
-				if n.Type == html.ElementNode && n.Data == "div" {
-					for _, attr := range n.Attr {
-						if attr.Key == "id" && attr.Val == "YouGridSlots" {
-							state = parserFoundSlotsStart
+			break
+		case parserFoundSlotsStart:
+			if n.Type == html.ElementNode && n.Data == "div" {
+				found := false
+				var temp uint64
+				for _, attr := range n.Attr {
+					if attr.Key == "id" && strings.Index(attr.Val, "YouTime") >= 0 {
+						found = true
+					} else if attr.Key == "data-time" {
+						temp, err = strconv.ParseUint(attr.Val, 10, 64)
+						if err != nil {
+							// error
+							return false, err
 						}
 					}
 				}
-				break
-			case parserFoundSlotsStart:
-				if n.Type == html.ElementNode && n.Data == "div" {
-					found := false
-					var temp uint64
-					for _, attr := range n.Attr {
-						if attr.Key == "id" && strings.Index(attr.Val, "YouTime") >= 0 {
-							found = true
-						} else if attr.Key == "data-time" {
-							temp, err = strconv.ParseUint(attr.Val, 10, 64)
-							if err != nil {
-								// error
-								return false, err
-							}
-						}
-					}
 
-					if found {
-						startTimestamp = temp
-						return false, nil
-					}
-				}
-				break
-			}
-
-			// Recurse!
-			for c := n.FirstChild; c != nil; c = c.NextSibling {
-				var cont bool
-				cont, err = f(c)
-				if !cont || err != nil {
-					return cont, err
+				if found {
+					ar.startTimestamp = temp
+					return false, nil
 				}
 			}
+			break
+		}
 
-			return true, nil
+		// Recurse!
+		for c := n.FirstChild; c != nil; c = c.NextSibling {
+			var cont bool
+			cont, err = f(c)
+			if !cont || err != nil {
+				return cont, err
+			}
 		}
-		_, err = f(doc)
-		if err != nil {
-			fmt.Println(err)
-			return nil, 0, 0
-		}
+
+		return true, nil
+	}
+	_, err = f(doc)
+	if err != nil {
+		return ar, err
 	}
 
 	// Custom parsing.
 	// 1. Id and name
 	stmts := strings.Split(availabilityJs[0], ";")
-	avail = make([]availability, len(stmts)/2)
+	ar.avail = make([]availability, len(stmts)/2)
 	for i, stmt := range stmts {
 		if strings.Index(stmt, "Names") >= 0 {
-			avail[i/2].name = strings.Trim(strings.Split(stmt, "=")[1], " '\"")
+			ar.avail[i/2].name = strings.Trim(strings.Split(stmt, "=")[1], " '\"")
 		} else if strings.Index(stmt, "IDs") >= 0 {
 			id, err := strconv.Atoi(strings.Trim(strings.Split(stmt, "=")[1], " '\""))
 			if err == nil {
-				avail[i/2].id = id
+				ar.avail[i/2].id = id
 			} else {
-				fmt.Println(err)
-				return nil, 0, 0
+				return ar, err
 			}
 		}
 	}
@@ -170,16 +151,16 @@ func getAvailability(i instance) (avail []availability, startTimestamp uint64, n
 		line := availabilityJs[hexIndex]
 		if strings.Index(line, "hexAvailability") >= 0 {
 			// TODO
-			nOfTimeslots = uint64(len(strings.Replace(line, "// hexAvailability: 0", "", 1)))
+			ar.nOfTimeslots = 4 * uint64(len(strings.Replace(line, "// hexAvailability: 0", "", 1)))
 			break
 		}
 	}
 
-	return avail, startTimestamp, nOfTimeslots
+	return ar, nil
 }
 
-func addTimestamps(i instance, avail []availability) {
-	url := fmt.Sprintf("https://when2meet.com/?%d-%s", i.id, i.code)
+func addTimestamps(ins instance, ar AvailabilityResponse) {
+	url := fmt.Sprintf("https://when2meet.com/?%d-%s", ins.id, ins.code)
 	resp, err := http.Get(url)
 	if err != nil {
 		// handle error
@@ -188,9 +169,46 @@ func addTimestamps(i instance, avail []availability) {
 	}
 	defer resp.Body.Close()
 
+	// TODO Setup
+	// one byte per 2 timeslots
+	for _, a := range ar.avail {
+		a.freeSlots = make([]byte, int(math.Ceil(float64(ar.nOfTimeslots/8))))
+	}
+
 	// Process line by line and if a line contains the required string, get it
 	lr := byline.NewReader(resp.Body)
 	lr.GrepByRegexp(regexp.MustCompile("AvailableAtSlot\\[[0-9]+\\]\\.push\\([0-9]+\\);"))
+	lr.EachString(func(line string) {
+		var slotIndex int
+		var userID uint64
+
+		// Extract the two numbers we're looking for
+		sec := strings.Split(line, "AvailableAtSlot[")
+		if len(sec) > 2 {
+			// TODO oops? more than one per line?
+		}
+		part := sec[1]
+		sec = strings.Split(part, "].push(")
+		slotIndex, err = strconv.Atoi(sec[0])
+		if err != nil {
+			// process error
+			fmt.Println(err)
+			return
+		}
+
+		part = sec[1]
+		sec = strings.Split(part, ");")
+		userID, err = strconv.ParseUint(sec[0], 10, 64)
+		if err != nil {
+			// process error
+			fmt.Println(err)
+			return
+		}
+
+		// TODO Calculate offset from start, set the hex
+		fmt.Println(slotIndex, userID)
+	})
+
 	result, err := lr.ReadAllSliceString()
 	if err != nil {
 		// handle error
@@ -198,13 +216,18 @@ func addTimestamps(i instance, avail []availability) {
 		return
 	}
 	fmt.Println("Lines with availability found: ", len(result))
+
+	// TODO
 }
 
 func main() {
 	// day := instance{6785667, "7INRG"}
 	date := instance{6939716, "nrhEh"}
 
-	avail, start, n := getAvailability(date)
-	addTimestamps(date, avail)
-	fmt.Println(avail, start, n)
+	ar, err := getAvailability(date)
+	if err != nil {
+		fmt.Println(err)
+	}
+	addTimestamps(date, ar)
+	fmt.Println(ar)
 }
